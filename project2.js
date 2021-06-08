7f88cc070f95e27e3d9f799367c6ecd643427719
Tazzy2018 <tazemania@hotmail.co.uk>
2021-06-08 19:04:45 -0400

    trying js code

--- project2.js
+++ project2.js
@@ -40,46 +40,24 @@
 
 // eventsApp.init();
 
-const formEl = document.querySelector('form');
-const city = []
-formEl.addEventListener('submit', function(event){
-  event.preventDefault();
-  const inputEl = document.querySelector('input[type=text]');
-    city.push(inputEl.value);
+// const formEl = document.querySelector('form');
+// const city = []
+// formEl.addEventListener('submit', function(event){
+//   event.preventDefault();
+//   const inputEl = document.querySelector('input[type=text]');
+//     city.push(inputEl.value);
   
   
-  // setting base url?
-const url = new URL('https://app.ticketmaster.com/discovery/v2/events');
-url.search.city = city;
-url.search = new URLSearchParams({
-  apikey: 'X4inC7WFIbCIszNWQJSMcDLteLVtz85Z',
-  city: [''],
-  //function that updates the city parameter
-
-});
-
-fetch(url)
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    console.log(data['_embedded']);
-    const eventsArray = data['_embedded'].events;
-    const eventName = data['_embedded'].events[0].name;
-    console.log(eventName);
-    document.querySelector('h3').innerText = eventName;
-  });
-  console.log(url.search);
-});
-
-
-// // setting base url?
+//   // setting base url?
 // const url = new URL('https://app.ticketmaster.com/discovery/v2/events');
+// url.search.city = city;
 // url.search = new URLSearchParams({
 //   apikey: 'X4inC7WFIbCIszNWQJSMcDLteLVtz85Z',
 //   city: [''],
 //   //function that updates the city parameter
+
 // });
+
 // fetch(url)
 //   .then(function (res) {
 //     return res.json();
@@ -91,3 +69,25 @@ fetch(url)
 //     console.log(eventName);
 //     document.querySelector('h3').innerText = eventName;
 //   });
+//   console.log(url.search);
+// });
+
+
+// setting base url?
+const url = new URL('https://app.ticketmaster.com/discovery/v2/events');
+url.search = new URLSearchParams({
+  apikey: 'X4inC7WFIbCIszNWQJSMcDLteLVtz85Z',
+  city: [''],
+  //function that updates the city parameter
+});
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data['_embedded']);
+    const eventsArray = data['_embedded'].events;
+    const eventName = data['_embedded'].events[0].name;
+    console.log(eventName);
+    document.querySelector('h3').innerText = eventName;
+  });
